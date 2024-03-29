f56474b5726769f02e9ab15e28901522416e7604
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-03-29 16:14:37 -0400

    added update workout and delete workout controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -44,11 +44,46 @@ const createWorkout = async(req,res)=>{
 };
 
 // delete a workout
+const deleteWorkout = async (req, res)=>{
+    try {
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({error: "No such document!"});
+        }
+        const workout = await Workout.findOneAndDelete({_id: id});
+        if(!workout){
+            return res.status(400).json({error: 'No such workout!'});
+        }
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 // update a workout
+const updateWorkout = async (req, res)=>{
+    try {
+        const { id } = req.params;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({error: 'No such workout!'});
+        }
+        const workout = await Workout.findOneAndUpdate({_id: id}, {
+            ...req.body
+        });
+        if(!workout){
+            return res.status(400).json({error: 'No such workout'});
+        }
+        res.status(200).json(workout);  
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+    
+}
 
 module.exports = {
     createWorkout,
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 }
