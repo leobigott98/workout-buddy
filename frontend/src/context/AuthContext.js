9785ec195c900fd0e83b13d93027c3f5868b5add
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-04-13 16:46:45 -0400

    add login check on authcontext

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext()
 
@@ -17,6 +17,17 @@ export const AuthContextProvider = ({ children })=>{
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
+
+    // when the component first renders, check if we have the user in localStorage
+
+    useEffect(()=>{
+        const user = JSON.parse(localStorage.getItem('user'))
+
+        if(user){
+            dispatch({ type: 'LOGIN', payload: user})
+        }
+    },[])
+
 
     console.log('AuthContext state: ', state)
 
